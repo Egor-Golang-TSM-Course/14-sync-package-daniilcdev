689f5705ea4f184e9ba1490f488ff26610feadf2
Daniil Cherepkov <groznybear@gmail.com>
2023-12-18 19:21:05 +0300

    feat: mt logbuffer

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"lesson14/mtBankAccount"
+	mtlogger "lesson14/mtLogger"
 )
 
 func main() {
 	done := make(chan struct{})
 
 	go mtBankAccount.StartConcurrentChanges(done)
+
+	<-done
+
+	go mtlogger.StartConcurrentLogging(done)
 
 	<-done
 
